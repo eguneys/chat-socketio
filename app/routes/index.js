c862f4f44d2f2404d694e8206f853835e593a725
eguneys <eguneys@hotmail.com>
2014-01-24 10:50:56 +0200

    Initial commit

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,7 +2,7 @@ var fs = require('fs');
 
 exports.index = function(req, res) {
 
-    fs.readFile('views/index.html', function(err, data) {
+    fs.readFile('public/index.html', function(err, data) {
 	if (err) throw err;
 	res.writeHead(200);
 	res.end(data);
